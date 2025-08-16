5f0bcadf1fcc324f97ff543b5da4a5b498c1aea3
Bandam-S <bandam.sandeep808030@gmail.com>
2025-08-16 17:40:08 +0530

    Add files via upload

---
+++ Python/Activities/Activity13.py
@@ -0,0 +1,8 @@
+def calculate_sum(numbers):
+	sum = 0
+	for number in numbers:
+		sum += number
+	return sum
+numList = [60, 20, 60, 90]
+result = calculate_sum(numList)
+print("The sum of all the elements is: " + str(result))
